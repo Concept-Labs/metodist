f4634b108a15cfc22d915c46fbbe50480f516829
StingerWin <witalikwin@gmail.com>
2018-04-02 21:01:54 +0300

    проба

---
+++ add.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Добавить</title>
+</head>
+<body>
+	<div style="background-position: 50% 50%;">Контент</div>
+</body>
+</html>
